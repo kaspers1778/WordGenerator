ef4130ef11d4faf97c41e7b751b3c7d020aecfd4
Pete Balachin <pet.balachin1778@gmail.com>
2019-03-04 21:06:31 +0200

    Create project

---
+++ WordGenerator/WordGenerator/WordGenerator.cpp
@@ -0,0 +1,45 @@
+﻿
+#include "pch.h"
+#include<cstdlib>
+#include <string>
+#include<time.h>
+#include <iostream>
+
+
+using namespace std;
+
+static string getWord(int lenght)
+{
+	string letters[] = { "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z" };
+	string word = "";
+	for (int i = 0;i < lenght;i++)
+	{
+		word += letters[rand() % 26];
+	}
+
+	return word;
+}
+
+int main()
+{
+	system("color 02");
+
+	srand(time(NULL));//Обязательно оставить эту строчку в main()
+	string wordToFind;
+	cout << "Write the word to find : ";
+	cin >> wordToFind;
+	int lenght = wordToFind.length();
+	int i = 0;
+	string word;
+	do
+	{
+		word = getWord(lenght);
+		cout << word << " ";
+		i++;
+	} while (word != wordToFind);
+	cout << endl << "The word is " << i << " word" << endl;
+
+	system("PAUSE");
+	return 0;
+}
+
